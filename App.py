87ce819b4d5d2bbd6efa0b928e77ed5e6db286b9
Abhishek Yadav <159156426+abhishekkumar62000@users.noreply.github.com>
2025-06-13 19:01:36 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,15 +1,15 @@
-import streamlit as st  
+import streamlit as st
 import google.generativeai as genai
-import os  
+import os
 import speech_recognition as sr
 import pyttsx3
-from gtts import gTTS 
+from gtts import gTTS
 from dotenv import load_dotenv
-from gramformer import Gramformer 
+from gramformer import Gramformer
 from textblob import TextBlob
 import time
 from io import BytesIO
-import base64 
+import base64
 import random
 import difflib
 import json
