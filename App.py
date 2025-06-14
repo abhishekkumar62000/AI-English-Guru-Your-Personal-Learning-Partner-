7fc4027c26c9c01a4d8919255220e2d549b6aa67
Abhishek Yadav <159156426+abhishekkumar62000@users.noreply.github.com>
2025-06-14 12:25:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import os
 import speech_recognition as sr 
 import pyttsx3
 from gtts import gTTS 
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
 from gramformer import Gramformer 
 from textblob import TextBlob  
 import time 
